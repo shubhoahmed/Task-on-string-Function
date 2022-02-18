edbf3ebdd5c09f3007ccdca2400dfd1c37fea7ff
Shubho Ahmed <59529614+shubhoahmed@users.noreply.github.com>
2022-02-18 16:40:10 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+
+//console.log("Hello World");
+
+//Task on String Function
+
+let firstName = prompt("Enter your First Name:");
+let lastName = prompt("Enter your last name: ");
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+
+let len = fullName.length;
+console.log("Total length is: " + len);
+console.log(fullName.toLocaleUpperCase());
+console.log(fullName.charAt(2));
+
+
+
+
