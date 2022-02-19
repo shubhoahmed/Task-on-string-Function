977800988d40fc40760c7c07f347fe4f35af5c3c
Shubho Ahmed <59529614+shubhoahmed@users.noreply.github.com>
2022-02-20 00:26:58 +0600

    Add files via upload

---
+++ control.js
@@ -0,0 +1,15 @@
+// if else statement
+let letter = prompt("Enter a Letter");
+
+letter = letter.toLowerCase();
+
+if(letter == "a" || letter == "e" || letter == "i" || letter == "o" || letter == "u")
+    console.log("Vowel");
+else
+    console.log("Consonalt");
+
+
+
+
+
+  
